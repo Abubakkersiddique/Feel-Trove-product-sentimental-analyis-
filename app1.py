4def85e23a8203fcf0bfad6c2a0768346fd157b2
Abubakker Siddique <abubakkersiddique34@gmail.com>
2025-06-03 06:58:08 +0530

    app1.py

--- app1.py
+++ app1.py
@@ -59,15 +59,6 @@ if st.sidebar.button("🚪 Logout"):
     st.rerun()
 
 # Theme
-if "theme" not in st.session_state:
-    st.session_state["theme"] = "light"
-
-def set_theme():
-    st.session_state["theme"] = "dark" if st.session_state["theme"] == "light" else "light"
-
-st.sidebar.button("Toggle Theme", on_click=set_theme)
-BG_COLOR = "#111" if st.session_state["theme"] == 'dark' else "#fff"
-TEXT_COLOR = "#fff" if st.session_state["theme"] == 'dark' else "#000"
 
 # --- SQL Load Function ---
 def load_sql_data(db_type, host, port, user, password, db_name, table_name):
